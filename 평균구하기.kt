2dd4afa85c046d1db547905c0b14712664eada2f
jiohjung98 <ckaquz98@naver.com>
2023-06-07 17:26:49 +0900

    평균구하기

---
+++ 평균구하기.kt
@@ -0,0 +1,15 @@
+// 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+
+// 제한사항
+// arr은 길이 1 이상, 100 이하인 배열입니다.
+// arr의 원소는 -10,000 이상 10,000 이하인 정수입니다.
+
+class Solution {
+    fun solution(arr: IntArray): Double {
+        var answer = 0.0
+        for (x in arr) {
+            answer += x
+        }
+        return answer / arr.size()
+    }
+}
